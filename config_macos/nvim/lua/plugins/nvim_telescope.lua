0a02c61f6c7e29d101ee8c6f2a74265465b86fd6
v1adhope <vladislavgardner@gmail.com>
2024-11-21 18:19:11 +0300

    Change telescope mapping

--- config_macos/nvim/lua/plugins/nvim_telescope.lua
+++ config_macos/nvim/lua/plugins/nvim_telescope.lua
@@ -13,6 +13,8 @@ local config = function()
 
   vim.keymap.set('n', 'ff', builtin.find_files, {})
   vim.keymap.set('n', 'fg', builtin.live_grep, {})
+  vim.keymap.set('n', 'fb', builtin.buffers, {})
+  vim.keymap.set('n', 'fh', builtin.help_tags, {})
 end
 
 return {
